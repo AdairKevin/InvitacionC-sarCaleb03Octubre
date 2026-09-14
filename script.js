4182bf66a3634ecf278ac26c182ad65dff4e33ad
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2026-09-14 13:53:48 -0600

    first

---
+++ script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const DATE_TARGET = new Date("10/03/2026 06:00 PM");
+
+  const SPAN_DAYS = document.querySelector("span#days");
+  const SPAN_HOURS = document.querySelector("span#hours");
+  const SPAN_MINUTES = document.querySelector("span#minutes");
+  const SPAN_SECONDS = document.querySelector("span#seconds");
+
+  const MILLISECONDS_OF_A_SECOND = 1000;
+  const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+  const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+  const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
+
+  function updateCountdown() {
+    const NOW = new Date();
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR,
+    );
+    const REMAINING_MINUTES = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE,
+    );
+    const REMAINING_SECONDS = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND,
+    );
+
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+  }
+
+  updateCountdown();
+
+  setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+});
+
+function PlayAudio() {
+  document.getElementById("musica1").play();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    // Obtener el código de la URL
+    const codigo = new URLSearchParams(window.location.search).get("id");
+
+    if (!codigo) {
+      document.getElementById("nombre").textContent = "Código no encontrado";
+      return;
+    }
+
+    // Leer el archivo JSON
+    const response = await fetch("./files/links.json");
+    const boletos = await response.json();
+
+    // Buscar invitado
+    const invitado = boletos[codigo];
+
+    if (!invitado) {
+      document.getElementById("nombre").textContent = "Invitación inválida";
+      return;
+    }
+
+    // Mostrar datos
+    document.getElementById("nombre").textContent = invitado.nombre;
+    document.getElementById("pasesAdulto").textContent = invitado.pasesAdulto;
+    document.getElementById("pasesNino").textContent = invitado.pasesNino;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("nombre").textContent = "Error al cargar datos";
+  }
+});
